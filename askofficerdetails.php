7ede0d5e82a7881cfab966fd67ad1255d83ab0a6
Ayush Bansal <ayushb6732@gmail.com>
2021-12-10 22:35:30 +0530

    Ask officer edited

--- askofficerdetails.php
+++ askofficerdetails.php
@@ -116,7 +116,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error)
 {
     die("Connection failed: " . $conn->connect_error);
-    header('Location: index.php');
+    header('Location: criminal.php');
 }
 else
 {  
